7bdf95538bfd4149a005413aa5689319c948cf1a
Eric <eric@commitchange.com>
2020-07-27 16:13:33 -0500

    Add esmodule interop so we can improve our import in src/money_format_helper.ts

--- src/money_format_helper.ts
+++ src/money_format_helper.ts
@@ -1,7 +1,7 @@
 // License: LGPL-3.0-or-later
 // from: https://github.com/jsillitoe/react-currency-input/blob/master/src/mask.js
 import { boundMethod } from "autobind-decorator";
-import isInteger  = require("lodash/isInteger");
+import isInteger from "lodash/isInteger";
 
 export interface MaskedAndRawValues {
   /**
