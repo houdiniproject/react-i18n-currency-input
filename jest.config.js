7aad065c02641ca0d9c7b581da4cb0427d91cf56
Eric <eric@commitchange.com>
2020-07-27 16:07:43 -0500

    Add jest-dom packages

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
+// License: LGPL-3.0-or-later
+// from: https://github.com/jsillitoe/react-currency-input/blob/master/test/setup.js
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node'
+  "testEnvironment": "jest-environment-jsdom-fifteen"
 };
