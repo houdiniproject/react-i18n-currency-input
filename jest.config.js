c914e98912ed92bb4887dffee765c1348f20398f
Eric Schultz <eric@commitchange.com>
2022-10-04 12:46:54 -0500

    Add react-16-* packages

--- jest.config.js
+++ jest.config.js
@@ -4,3 +4,13 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom'
 };
+
+if(process.env.USE_REACT_16 === 'true'){
+  module.exports.cacheDirectory = '.cache/jest-cache-react-16'
+  module.exports.moduleNameMapper = {
+    ...module.exports.moduleNameMapper,
+    '^react-is((\\/.*)?)$': 'react-is-16$1',
+    '^react-dom((\\/.*)?)$': 'react-dom-16$1',
+    '^react((\\/.*)?)$': 'react-16$1'
+  }
+}
