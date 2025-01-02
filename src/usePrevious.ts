3c36d8fed22e8c54ada6fa595d82b8b0cd9db034
Eric Schultz <eric@wwahammy.com>
2025-01-02 16:56:38 -0600

    Correct the typing for usePrevious

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -4,7 +4,7 @@
 import { useEffect, useRef } from 'react';
 
 export default function usePrevious<T>(state: T): T | undefined {
-  const ref = useRef<T>();
+  const ref = useRef<T>(undefined);
 
   useEffect(() => {
     ref.current = state;
