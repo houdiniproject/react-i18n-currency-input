1f7a82dd97a4da11e42874e41466f16345d70170
Eric Schultz <eric@wwahammy.com>
2025-01-03 20:16:14 -0600

    fix: Move release into its own file

---
+++ release.config.mjs
@@ -0,0 +1,20 @@
+/**
+ * @type {import('semantic-release').GlobalConfig}
+ */
+export default {
+    "branches": [
+        "main"
+    ],
+    plugins: [
+        "@semantic-release/commit-analyzer",
+        "@semantic-release/release-notes-generator",
+        "@semantic-release/changelog",
+        "@semantic-release/npm",
+        [
+            "@semantic-release/git", {
+                "assets": ["dist/**/*.{js,css}", "docs", "package.json"],
+                "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+            }
+        ]
+    ]
+};
